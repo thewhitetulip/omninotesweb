241125b4f78799e9bc303793315e8ff966064348
Suraj Patil <sapatil@live.com>
2015-01-18 21:25:24 +0530

    initial commit

---
+++ includes/js/script.js
@@ -0,0 +1,16 @@
+/*
+
+This is a javascript file for omninotesweb
+============
+
+Author:  Suraj patil
+Updated: January 2015
+
+*/
+
+$(document).ready(function(){
+  $("#note123").click(function(){
+    $(this).fadeOut();
+
+  });
+});
